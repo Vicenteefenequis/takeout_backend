28ecc42bf66956f5ae9dff2e65ab2f5be1df4f65
Vicente Nascimento <vicente@DESKTOP-MS9JDQQ.localdomain>
2021-10-13 21:38:50 -0300

    feat: create listener of service port 8080

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"takeout-backend/application/controllers"
+
+)
+
+func main() {
+
+	log.Fatal(http.ListenAndServe(":8080", controllers.NewRootController()))
+}
