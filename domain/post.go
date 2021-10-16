944b1eafab35ea893eebe84c7321950d1531a4aa
Vicente Nascimento <vicente19981@live.com>
2021-10-16 12:51:09 -0300

    refactor: added omitempty property in post struct

--- domain/post.go
+++ domain/post.go
@@ -1,10 +1,10 @@
 package domain
 
 type Post struct {
-	NameUser    string `bson:"name_user" json:"name_user"`
-	TypePost    string `bson:"type_post" json:"type_post"`
-	Visibility  string `bson:"visibility" json:"visibility"`
-	CaseStatus  string `bson:"case_status" json:"case_status"`
-	Image       string `bson:"image" json:"image"`
-	Description string `bson:"description" json:"description"`
+	NameUser    string `bson:"name_user,omitempty" json:"name_user,omitempty"`
+	TypePost    string `bson:"type_post,omitempty" json:"type_post,omitempty"`
+	Visibility  string `bson:"visibility,omitempty" json:"visibility,omitempty"`
+	CaseStatus  string `bson:"case_status,omitempty" json:"case_status,omitempty"`
+	Image       string `bson:"image,omitempty" json:"image,omitempty"`
+	Description string `bson:"description,omitempty" json:"description,omitempty"`
 }
