4b416635a4e26c1e21b3bd396cb6dffae2bef315
Vicente Nascimento <vicente19981@live.com>
2021-10-16 12:51:57 -0300

    feat: added route update of post

--- application/controllers/root-controller.go
+++ application/controllers/root-controller.go
@@ -2,19 +2,16 @@ package controllers
 
 import (
 	"github.com/gorilla/mux"
-
 )
 
-
-
-func NewRootController() *mux.Router  {
+func NewRootController() *mux.Router {
 	postController := NewPostController()
 
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/",postController.GetAll).Methods("GET")
+	router.HandleFunc("/", postController.GetAll).Methods("GET")
 	router.HandleFunc("/{id}", postController.GetById).Methods("GET")
-	router.HandleFunc("/",postController.Create).Methods("POST")
-	router.HandleFunc("/{id}",postController.Delete).Methods("DELETE")
-
+	router.HandleFunc("/", postController.Create).Methods("POST")
+	router.HandleFunc("/{id}", postController.Delete).Methods("DELETE")
+	router.HandleFunc("/{id}", postController.Update).Methods("PUT")
 	return router
 }
