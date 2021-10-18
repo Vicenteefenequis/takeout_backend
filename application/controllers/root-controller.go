70c69ff0edd5dfb99387d8389468557f1dbc3aa1
Vicente Nascimento <vicente19981@live.com>
2021-10-17 22:20:54 -0300

    refactor: change route of post

--- application/controllers/root-controller.go
+++ application/controllers/root-controller.go
@@ -8,10 +8,10 @@ func NewRootController() *mux.Router {
 	postController := NewPostController()
 
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", postController.GetAll).Methods("GET")
-	router.HandleFunc("/{id}", postController.GetById).Methods("GET")
-	router.HandleFunc("/", postController.Create).Methods("POST")
-	router.HandleFunc("/{id}", postController.Delete).Methods("DELETE")
-	router.HandleFunc("/{id}", postController.Update).Methods("PUT")
+	router.HandleFunc("/post", postController.GetAll).Methods("GET")
+	router.HandleFunc("/post", postController.Create).Methods("POST")
+	router.HandleFunc("/post/{id}", postController.GetById).Methods("GET")
+	router.HandleFunc("/post/{id}", postController.Delete).Methods("DELETE")
+	router.HandleFunc("/post/{id}", postController.Update).Methods("PUT")
 	return router
 }
--- main.go
+++ main.go
@@ -4,10 +4,8 @@ import (
 	"log"
 	"net/http"
 	"takeout-backend/application/controllers"
-
 )
 
 func main() {
-
 	log.Fatal(http.ListenAndServe(":8080", controllers.NewRootController()))
 }
